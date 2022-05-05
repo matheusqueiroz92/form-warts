c4d3b0bc5d677c8f0834f07bee75d2485a46004e
Felipe <fe.donatto@outlook.com>
2022-05-05 16:53:29 -0300

    arrumado erro no 3

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function loginAlert() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  if (email === 'tryber@teste.com' && password === 123456) {
+  if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
