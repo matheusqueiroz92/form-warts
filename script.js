4838f28a2cf9eb16e6dfcabefc5ae6a9baf8adaf
Felipe <fe.donatto@outlook.com>
2022-05-09 21:47:19 -0300

    ultimo commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const loginButton = document.getElementById('login');
+const buttonEnviar = document.getElementById('submit-btn');
+const formCadastrado = document.getElementById('evaluation-form');
+const inputName = document.getElementById('input-name');
+const inputLastname = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const house = document.querySelector('#house');
+const inputFamily = document.querySelectorAll('.radio');
+const conteudo = document.querySelectorAll('.subject');
+const avalia = document.querySelectorAll('.avaliacao');
 
 function loginAlert() {
   const email = document.getElementById('email').value;
@@ -25,8 +34,58 @@ checkbox.addEventListener('click', enableButton);
 
 const textarea = document.querySelector('#textarea');
 const counter = document.getElementById('counter');
+
 function countLenght() {
   counter.innerText = 500 - textarea.value.length;
 }
 
 textarea.addEventListener('input', countLenght);
+
+console.log(conteudo);
+
+function getInputRadio() { // pega o valor do input radio family
+  for (let i = 0; i < inputFamily.length; i += 1) {
+    if (inputFamily[i].checked) {
+      return inputFamily[i].value;
+    }
+  }
+}
+function getInputContent() { // pega o valor do input content
+  let materias = '';
+  for (let i2 = 0; i2 < conteudo.length; i2 += 1) {
+    if (conteudo[i2].checked === true) {
+      materias += `${conteudo[i2].value}, `;
+    }
+  }
+  return materias;
+}
+function getInputAvalia() { // pega o valor do input avaliação
+  for (let i3 = 0; i3 < avalia.length; i3 += 1) {
+    if (avalia[i3].checked) {
+      return avalia[i3].value;
+    }
+  }
+}
+function montaForm() {
+  const valueHouse = house.options[house.selectedIndex].value; // pega o valor da House
+  formCadastrado.innerHTML = '';
+  const textForm = document.createElement('p');
+  textForm.innerText = `Nome: ${inputName.value} ${inputLastname.value}
+  Email: ${inputEmail.value}
+  Família: ${getInputRadio()}
+  Casa: ${valueHouse}
+  Matérias: ${getInputContent()}
+  Avaliação: ${getInputAvalia()}
+  Observações: ${textarea.value}`;
+  formCadastrado.appendChild(textForm);
+}
+
+function alteraForm() {
+  getInputRadio();
+  getInputContent();
+  getInputAvalia();
+  montaForm();
+}
+// reduzi o tamanho da função pelo limite do lint
+
+buttonEnviar.addEventListener('click', alteraForm);
